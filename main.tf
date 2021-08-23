5cea43de9599a43b88ec41ce703d095f85f8f726
Mariano Korman <mkormanc@gmail.com>
2021-08-23 16:36:19 +0100

    Added a web server script
    - Requires a security group

--- main.tf
+++ main.tf
@@ -5,8 +5,31 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-04f1bbad31585405d"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [ aws_security_group.web_access.id ]
+
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello, World" > index.html
+        nohup busybox httpd -f -p 8080 &
+        EOF
 
     tags = {
         Name = "created-by-terraform"
     }
+}
+
+resource "aws_security_group" "web_access" {
+    name = "terraform-example-security-group"   
+
+    ingress = [ {
+      from_port = 8080
+      to_port = 8080
+      cidr_blocks = [ "0.0.0.0/0" ]
+      protocol = "tcp"
+      description = "Allows connections on port 8080"
+      ipv6_cidr_blocks = [  ]
+      prefix_list_ids = [  ]
+      security_groups = [  ]
+      self = false
+    } ]
 }
