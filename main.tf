1748d4605b3c839fa8ff3221cdc958f79789eb94
Mariano Korman <mkormanc@gmail.com>
2021-08-24 12:33:50 +0100

    Replaced the single instance with a load balancer
    - Added ELB
    - Added autoscaling group
    - Added target listener and target group

--- main.tf
+++ main.tf
@@ -14,15 +14,55 @@ variable "ssh_port" {
     default = 22
 }
 
-output "public_ip" {
-  value = aws_instance.example.public_ip
-  description = "The public IP address of our EC2 instance"
+output "alb_dns_name" {
+  value = aws_alb.example_alb.dns_name
+  description = "The domain name of the load balancer"
 }
 
-resource "aws_instance" "example" {
-    ami = "ami-04f1bbad31585405d"
+resource "aws_alb" "example_alb" {
+    name = "terraformed-alb"
+    load_balancer_type = "application"
+    subnets = data.aws_subnet_ids.default.ids
+    security_groups = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_alb.example_alb.arn
+    port = 80
+    protocol = "HTTP"
+
+    default_action {
+        type = "fixed-response"
+
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "404: page not found"
+            status_code = 404
+        }
+    }
+}
+
+resource "aws_lb_target_group" "asg" {
+    name = "terraform-asg-example"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+    
+    health_check {
+        path = "/"
+        protocol = "HTTP"
+        matcher = "200"
+        interval = 15
+        timeout = 3
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+    }
+}
+
+resource "aws_launch_configuration" "example_launch_config" {
+    image_id = "ami-04f1bbad31585405d"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [ aws_security_group.web_access.id ]
+    security_groups = [ aws_security_group.web_access.id ]
 
     user_data = <<EOF
         #!/bin/bash
@@ -31,8 +71,41 @@ resource "aws_instance" "example" {
         echo "<h1>Hello, World</h1>" > /var/www/html/index.html
         EOF
 
-    tags = {
-        Name = "created-by-terraform"
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+resource "aws_autoscaling_group" "example_autoscaling_grop" {
+    launch_configuration = aws_launch_configuration.example_launch_config.name
+    vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+    target_group_arns = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key = "Name"
+        value = "terraform-asg-example"
+        propagate_at_launch = true
+    }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+    listener_arn = aws_lb_listener.http.arn
+    priority = 100
+
+    condition {
+        path_pattern {
+            values = ["*"]
+        }
+    }
+
+    action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.asg.arn
     }
 }
 
@@ -55,12 +128,35 @@ resource "aws_security_group" "web_access" {
       to_port = 22
       cidr_blocks = [ "0.0.0.0/0" ]
       protocol = "tcp"
-      description = "Allows incoming TCP connections on the configured server port"
+      description = "Allows incoming TCP connections for ssh"
       ipv6_cidr_blocks = [  ]
       prefix_list_ids = [  ]
       security_groups = [  ]
       self = false
     }  ]
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "alb" {
+    name = "terraform-alb-security-group"   
+
+    ingress = [ {
+      from_port = 80
+      to_port = 80
+      cidr_blocks = [ "0.0.0.0/0" ]
+      protocol = "tcp"
+      description = "Allows incoming TCP connections on the configured server port"
+      ipv6_cidr_blocks = [  ]
+      prefix_list_ids = [  ]
+      security_groups = [  ]
+      self = false
+    } ]
 
     egress {
         from_port = 0
