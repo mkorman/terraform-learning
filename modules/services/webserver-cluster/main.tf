1094436dc8b19f8b81d69d6d28ea898cb66d18ee
Mariano Korman <mkormanc@gmail.com>
2021-08-31 18:01:29 +0100

    Refactoring
    - Removed unused/unnecessary resources
    - Moved security group rules to separate resources

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -25,6 +25,7 @@ locals {
   any_port     = 0
   any_protocol = -1
   tcp_protocol = "tcp"
+  http_protocol = "HTTP"
   all_ips       = ["0.0.0.0/0"]
 }
 
@@ -54,12 +55,12 @@ resource "aws_lb_listener" "http" {
 resource "aws_lb_target_group" "asg" {
   name     = "terraform-target-group"
   port     = var.server_port
-  protocol = "HTTP"
+  protocol = local.http_protocol
   vpc_id   = data.aws_vpc.default.id
 
   health_check {
     path                = "/"
-    protocol            = "HTTP"
+    protocol            = local.http_protocol
     matcher             = "200"
     interval            = 15
     timeout             = 3
@@ -71,7 +72,7 @@ resource "aws_lb_target_group" "asg" {
 resource "aws_launch_configuration" "example_launch_config" {
   image_id        = "ami-04f1bbad31585405d"
   instance_type   = var.instance_type
-  security_groups = [aws_security_group.web_access.id]
+  security_groups = [aws_security_group.alb.id]
   user_data       = data.template_file.user_data.rendered
 
   lifecycle {
@@ -112,10 +113,13 @@ resource "aws_lb_listener_rule" "asg" {
   }
 }
 
-resource "aws_security_group" "web_access" {
-  name = "${var.cluster_name}-security-group"
+resource "aws_security_group" "alb" {
+  name = "${var.cluster_name}-alb"
+}
 
-  ingress = [{
+resource "aws_security_group_rule" "allow_http_inbound" {
+    type = "ingress"
+    security_group_id = aws_security_group.alb.id
     from_port        = local.http_port
     to_port          = local.http_port
     cidr_blocks      = local.all_ips
@@ -123,50 +127,27 @@ resource "aws_security_group" "web_access" {
     description      = "Allows incoming TCP connections on the configured server port"
     ipv6_cidr_blocks = []
     prefix_list_ids  = []
-    security_groups  = []
-    self             = false
-    },
-    {
-      from_port        = local.ssh_port
-      to_port          = local.ssh_port
-      cidr_blocks      = local.all_ips
-      protocol         = local.tcp_protocol
-      description      = "Allows incoming TCP connections for ssh"
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-  }]
-
-  egress {
-    from_port   = local.any_port
-    to_port     = local.any_port
-    protocol    = local.any_protocol
-    cidr_blocks = local.all_ips
-  }
 }
 
-resource "aws_security_group" "alb" {
-  name = "${var.cluster_name}-alb"
-
-  ingress = [{
-    from_port        = local.http_port
-    to_port          = local.http_port
+resource "aws_security_group_rule" "allow_ssh_inbound" {
+    type = "ingress"
+    security_group_id = aws_security_group.alb.id
+    from_port        = local.ssh_port
+    to_port          = local.ssh_port
     cidr_blocks      = local.all_ips
-    protocol         = "tcp"
+    protocol         = local.tcp_protocol
     description      = "Allows incoming TCP connections on the configured server port"
     ipv6_cidr_blocks = []
     prefix_list_ids  = []
-    security_groups  = []
-    self             = false
-  }]
+}
 
-  egress {
+resource "aws_security_group_rule" "allow_all_outbound" {
+    type = "egress"
+    security_group_id = aws_security_group.alb.id
     from_port   = local.any_port
     to_port     = local.any_port
     protocol    = local.any_protocol
     cidr_blocks = local.all_ips
-  }
 }
 
 data "aws_vpc" "default" {
--- modules/services/webserver-cluster/outputs.tf
+++ modules/services/webserver-cluster/outputs.tf
@@ -3,3 +3,8 @@ output "alb_dns_name" {
   value       = aws_alb.example_alb.dns_name
   description = "The domain name of the load balancer"
 }
+
+output "alb_security_group_id" {
+  value = aws_security_group.alb.id
+  description = "The ID of the Security Group attached to the load balancer"
+}
