36f0f5ccab766e80dda476a5f6a88260dc56190e
Mariano Korman <mkormanc@gmail.com>
2021-08-31 14:32:05 +0100

    Added more variables to the module
    - Also ran `terraform fmt`

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -1,195 +1,204 @@
 terraform {
-    backend "s3" {
-        bucket = var.db_remote_state_bucket
-        key = var.db_remote_state_key
-        region = "eu-west-2"
-        encrypt = true
-    }
+  backend "s3" {
+    bucket  = var.db_remote_state_bucket
+    key     = var.db_remote_state_key
+    region  = "eu-west-2"
+    encrypt = true
+  }
 }
 
 variable "server_port" {
-    description = "The port the server will use for HTTP requests"
-    type = number
-    default = 8080
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
 }
 
 variable "ssh_port" {
-    description = "The port the server will use for ssh"
-    type = number
-    default = 22
+  description = "The port the server will use for ssh"
+  type        = number
+  default     = 22
 }
 
 output "alb_dns_name" {
-  value = aws_alb.example_alb.dns_name
+  value       = aws_alb.example_alb.dns_name
   description = "The domain name of the load balancer"
 }
 
+locals {
+  ssh_port     = 22
+  http_port    = 80
+  any_port     = 0
+  any_protocol = -1
+  tcp_protocol = "tcp"
+  all_ips       = ["0.0.0.0/0"]
+}
+
 resource "aws_alb" "example_alb" {
-    name = "${var.cluster_name}"
-    load_balancer_type = "application"
-    subnets = data.aws_subnet_ids.default.ids
-    security_groups = [aws_security_group.alb.id]
+  name               = var.cluster_name
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
 }
 
 resource "aws_lb_listener" "http" {
-    load_balancer_arn = aws_alb.example_alb.arn
-    port = 80
-    protocol = "HTTP"
+  load_balancer_arn = aws_alb.example_alb.arn
+  port              = local.http_port
+  protocol          = "HTTP"
 
-    default_action {
-        type = "fixed-response"
+  default_action {
+    type = "fixed-response"
 
-        fixed_response {
-            content_type = "text/plain"
-            message_body = "404: page not found"
-            status_code = 404
-        }
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
     }
+  }
 }
 
 resource "aws_lb_target_group" "asg" {
-    name = "terraform-target-group"
-    port = var.server_port
-    protocol = "HTTP"
-    vpc_id = data.aws_vpc.default.id
-    
-    health_check {
-        path = "/"
-        protocol = "HTTP"
-        matcher = "200"
-        interval = 15
-        timeout = 3
-        healthy_threshold = 2
-        unhealthy_threshold = 2
-    }
+  name     = "terraform-target-group"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 }
 
 resource "aws_launch_configuration" "example_launch_config" {
-    image_id = "ami-04f1bbad31585405d"
-    instance_type = var.instance_type
-    security_groups = [ aws_security_group.web_access.id ]
-    user_data = data.template_file.user_data.rendered
+  image_id        = "ami-04f1bbad31585405d"
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.web_access.id]
+  user_data       = data.template_file.user_data.rendered
 
-    lifecycle {
-        create_before_destroy = true
-    }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_autoscaling_group" "example_autoscaling_grop" {
-    launch_configuration = aws_launch_configuration.example_launch_config.name
-    vpc_zone_identifier = data.aws_subnet_ids.default.ids
+  launch_configuration = aws_launch_configuration.example_launch_config.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
 
-    target_group_arns = [aws_lb_target_group.asg.arn]
-    health_check_type = "ELB"
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
-    min_size = var.min_size
-    max_size = var.max_size
+  min_size = var.min_size
+  max_size = var.max_size
 
-    tag {
-        key = "Name"
-        value = var.cluster_name
-        propagate_at_launch = true
-    }
+  tag {
+    key                 = "Name"
+    value               = var.cluster_name
+    propagate_at_launch = true
+  }
 }
 
 resource "aws_lb_listener_rule" "asg" {
-    listener_arn = aws_lb_listener.http.arn
-    priority = 100
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
 
-    condition {
-        path_pattern {
-            values = ["*"]
-        }
+  condition {
+    path_pattern {
+      values = ["*"]
     }
+  }
 
-    action {
-        type = "forward"
-        target_group_arn = aws_lb_target_group.asg.arn
-    }
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
 }
 
 resource "aws_security_group" "web_access" {
-    name = "${var.cluster_name}-security-group"   
+  name = "${var.cluster_name}-security-group"
 
-    ingress = [ {
-      from_port = var.server_port
-      to_port = var.server_port
-      cidr_blocks = [ "0.0.0.0/0" ]
-      protocol = "tcp"
-      description = "Allows incoming TCP connections on the configured server port"
-      ipv6_cidr_blocks = [  ]
-      prefix_list_ids = [  ]
-      security_groups = [  ]
-      self = false
+  ingress = [{
+    from_port        = local.http_port
+    to_port          = local.http_port
+    cidr_blocks      = local.all_ips
+    protocol         = local.tcp_protocol
+    description      = "Allows incoming TCP connections on the configured server port"
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
     },
     {
-      from_port = 22
-      to_port = 22
-      cidr_blocks = [ "0.0.0.0/0" ]
-      protocol = "tcp"
-      description = "Allows incoming TCP connections for ssh"
-      ipv6_cidr_blocks = [  ]
-      prefix_list_ids = [  ]
-      security_groups = [  ]
-      self = false
-    }  ]
+      from_port        = local.ssh_port
+      to_port          = local.ssh_port
+      cidr_blocks      = local.all_ips
+      protocol         = local.tcp_protocol
+      description      = "Allows incoming TCP connections for ssh"
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+  }]
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = local.any_port
+    to_port     = local.any_port
+    protocol    = local.any_protocol
+    cidr_blocks = local.all_ips
+  }
 }
 
 resource "aws_security_group" "alb" {
-    name = "${var.cluster_name}-alb"   
+  name = "${var.cluster_name}-alb"
 
-    ingress = [ {
-      from_port = 80
-      to_port = 80
-      cidr_blocks = [ "0.0.0.0/0" ]
-      protocol = "tcp"
-      description = "Allows incoming TCP connections on the configured server port"
-      ipv6_cidr_blocks = [  ]
-      prefix_list_ids = [  ]
-      security_groups = [  ]
-      self = false
-    } ]
+  ingress = [{
+    from_port        = local.http_port
+    to_port          = local.http_port
+    cidr_blocks      = local.all_ips
+    protocol         = "tcp"
+    description      = "Allows incoming TCP connections on the configured server port"
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    security_groups  = []
+    self             = false
+  }]
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = local.any_port
+    to_port     = local.any_port
+    protocol    = local.any_protocol
+    cidr_blocks = local.all_ips
+  }
 }
 
 data "aws_vpc" "default" {
-    default = true
+  default = true
 }
 
 data "aws_subnet_ids" "default" {
-    vpc_id = data.aws_vpc.default.id
+  vpc_id = data.aws_vpc.default.id
 }
 
 # Read data from the Terraform backend from the DB
 data "terraform_remote_state" "db" {
-    backend = "s3"
+  backend = "s3"
 
-    config = {
-        bucket = var.db_remote_state_bucket
-        key = var.db_remote_state_key
-        region = "eu-west-2"
-    }
+  config = {
+    bucket = var.db_remote_state_bucket
+    key    = var.db_remote_state_key
+    region = "eu-west-2"
+  }
 }
 
 data "template_file" "user_data" {
-    template = file ("./user-data.sh")
+  template = file("./user-data.sh")
 
-    vars = {
-        server_port = var.server_port
-        db_address  = data.terraform_remote_state.db.outputs.address
-        db_port  = data.terraform_remote_state.db.outputs.port
-    }
+  vars = {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
+  }
 }
